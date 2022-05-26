6b374b484abef74752a242d1ea2b61d8ae243df9
Problematik <102496559+Problematik1@users.noreply.github.com>
2022-05-26 21:09:40 +0300

    Add files via upload

---
+++ pinger.py
@@ -0,0 +1,19 @@
+from colorama import Fore, init
+import requests
+import os
+init()
+os.system('title Pinger')
+os.system('mode 55, 25')
+color = Fore.LIGHTWHITE_EX
+downed = Fore.RED
+connected = Fore.LIGHTGREEN_EX
+url = input(f'{color} Url: ')
+os.system(f'title Pinging ~{url}~ ')
+while True:
+ r = requests.get(url)
+ if r.status_code == 200:
+     print(f'{connected} Connected: {r.status_code}')
+ elif r.status_code != 200:
+     print(f'{downed} Connection timed out: {r.status_code}')    
+
+ input()     
